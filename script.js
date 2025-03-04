fd4bc633e17f0281e7b9113c613ae4af596827ae
Hòa Nam 12F <hoanam224@gmail.com>
2025-03-04 07:26:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function.login()
+{
+  let u == username.value 
+  let p == password.value
+  if ( u =="1" && p =="1")
+  {
+    window.open
+    document.getElementById(compiler).style.display="block";
+  }
+  else
+    document.write("Wrong username or password")
+}
